9dc2176ec6c19e21081edc413abae9c85423538f
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-07 13:26:43 -0700

    Add Plugin

---
+++ WhoAmIAgain/WhoAmIAgain.cs
@@ -0,0 +1,46 @@
+﻿using Dalamud.Game.Gui.Dtr;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace WhereAmIAgain;
+
+public sealed class WhoAmIAgainPlugin : IDalamudPlugin
+{
+    private readonly IClientState clientState;
+    private readonly IDtrBar dtrBar;
+    
+    private DtrBarEntry? dtrBarEntry;
+    
+    public WhoAmIAgainPlugin(IClientState clientStateService, IDtrBar dtrService)
+    {
+        clientState = clientStateService;
+        dtrBar = dtrService;
+        
+        if (clientState.IsLoggedIn) OnLogin();
+        
+        clientState.Login += OnLogin;
+        clientState.Logout += OnLogout;
+    }
+
+    private void OnLogin()
+    {
+        if (dtrBar.Get("WhoAmIAgain") is not { } entry) return;
+
+        dtrBarEntry = entry;
+        dtrBarEntry.Text = clientState.LocalPlayer?.Name;
+        dtrBarEntry.Tooltip = new SeStringBuilder().AddText($"You are: {clientState.LocalPlayer?.Name ?? "Unknown"}").Build();
+    }
+
+    private void OnLogout()
+    {
+        dtrBarEntry?.Remove();
+        dtrBarEntry = null;
+    }
+
+    public void Dispose()
+    {
+        clientState.Login -= OnLogin;
+        clientState.Logout -= OnLogout;
+    }
+}
